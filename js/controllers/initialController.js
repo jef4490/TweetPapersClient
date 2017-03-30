cd1ff4d6faf9631b0fa3081fa819cbeccba8c8c9
MartinCrane <martin.crane@gmail.com>
2017-03-30 15:48:27 -0400

    master start;

---
+++ js/controllers/initialController.js
@@ -0,0 +1,11 @@
+class InitialController {
+  // constructor($target) {
+  //   this.$target = $target
+  //   this.tweets = essay.tweets
+  //   this.render()
+  // }
+  //
+  // render(){
+  //   return EssayTweetsView.renderTweets(this.$target, this.tweets)
+  // }
+}
---
+++ js/views/initialView.js
@@ -0,0 +1,8 @@
+class InitialView {
+  static displayInstructions (event){
+    if (event.target.classList[0] == "render"){
+    } else if (event.target.classList[0] == "write") {
+    } else {
+    }
+  }
+}
