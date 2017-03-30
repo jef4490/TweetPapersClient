b3c6d6c451db6738d23be05b900409ca9d59a3a0
jef4490 <jeffrey.hechler@gmail.com>
2017-03-30 12:29:10 -0400

    fixed splitting sentences bug

--- js/models/essay.js
+++ js/models/essay.js
@@ -15,14 +15,17 @@ class Essay {
     let i = 0
     while (i < this.sentences.length) {
       if (this.sentences[i].length > 135) {
-        let midpoint = Math.floor(this.sentences[i].length)
-        this.tweets.push(new EssayTweet(this.sentences.slice(0, midpoint)))
-        this.tweets.push(new EssayTweet(this.sentences.slice(midpoint, this.sentences[i].length)))
+        let midpoint = Math.floor(this.sentences[i].length/2)
+          while (this.sentences[i][midpoint] != " ") {
+            midpoint ++
+          }
+        this.tweets.push(new EssayTweet(this.sentences[i].slice(0, midpoint)))
+        this.tweets.push(new EssayTweet(this.sentences[i].slice(midpoint, this.sentences[i].length)))
         i++
       }
       let current = ""
       while (this.sentences[i] && current.length + this.sentences[i].length <= 135) {
-        current += this.sentences[i]
+        current += (" " + this.sentences[i])
         i++
       }
       let tweet = new EssayTweet(current)
