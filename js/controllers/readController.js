a710107070cbf466e29027215243139ce2fcb5b0
MartinCrane <martin.crane@gmail.com>
2017-03-30 16:35:26 -0400

    finished initial view

---
+++ js/controllers/readController.js
@@ -0,0 +1,23 @@
+class ReadController {
+  constructor() {
+    ReadView.renderReadView()
+    this.attachListeners()
+  }
+
+  attachListeners() {
+    let tweet = ''
+    let $target = $('.tweet-essay-window')
+    $('.tweet-url-form').on('submit', () => {
+      event.preventDefault()
+      let url = $('#tweet-url-text').val()
+      let rawTweetData = new TweetAPIController(url)
+      rawTweetData.retrieveTweet().then((rawTweetData) => {
+        tweet = new Tweet(rawTweetData)
+        TweetAPIController.retrieveTimeline(tweet).then((timeline) => {
+          let timelineObject = new Timeline(timeline, tweet)
+          let x = new TweetEssayController($target, timelineObject)
+        })
+      })
+    })
+  }
+}
