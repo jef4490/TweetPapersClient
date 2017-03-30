242440ff9db72de2ab0ea038ddae6e518cb99181
MartinCrane <martin.crane@gmail.com>
2017-03-30 11:00:24 -0400

    bug fix

--- js/models/tweet.js
+++ js/models/tweet.js
@@ -25,14 +25,14 @@ class Tweet {
       if (regexFractionStart.test(this.text)) {
         this.regex = regexFractionStart
         return this.tweetCount = this.text.match(this.regex)[0].match(/\d+/)[0]
+      } else if (regexNumberStart.test(this.text)) {
+        this.regex = regexNumberStart
+        return this.tweetCount = this.text.match(this.regex)[0].match(/\d+/)[0]
+      } else if (regexFractionEnd.test(this.text)) {
+        this.regex = regexFractionEnd
+        return this.tweetCount = this.text.match(this.regex)[0].match(/\d+/)[0]
       } else if (regexNumberEnd.test(this.text)) {
        this.regex = regexNumberEnd
-       return this.tweetCount = this.text.match(this.regex)[0].match(/\d+/)[0]
-     } else if (regexNumberStart.test(this.text)) {
-       this.regex = regexNumberStart
-       return this.tweetCount = this.text.match(this.regex)[0].match(/\d+/)[0]
-     } else if (regexFractionEnd.test(this.text)) {
-       this.regex = regexFractionEnd
        return this.tweetCount = this.text.match(this.regex)[0].match(/\d+/)[0]
      } else {
        alert("error!")
